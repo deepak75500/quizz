ad458cb3dbd9fddd3af624d7925953347dc82be7
DEEPAK S <162658459+deepak75500@users.noreply.github.com>
2026-09-10 21:24:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 import os
 
-PLAYWRIGHT_BROWSERS_PATH=/tmp/ms-playwright
+
 
 import traceback
 from pathlib import Path
